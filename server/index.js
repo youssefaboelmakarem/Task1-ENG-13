53d2c77b3751097e60367428973647ac27d62be9
youssefaboelmakarem <47251605+youssefaboelmakarem@users.noreply.github.com>
2019-02-22 14:37:29 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const students = [
     { id: "21-094123", name: "Mathew White", github_username: "matheww", email: "matheww@whatever.com" },
     { id: "15-10312", name: "Dom Sundle", github_username: "domss", email: "domss.whatever.com" },
     { id: "7223", name: "Gehad Ismail", github_username: "Gehad93", email: "gehad.ismail@guc.edu.eg" },
-	{ id: "40-3279", name: "Youssef Abo El Makarem", github_username: "youssefaboelmakarem", email: "youssef.elguindy@student.guc.edu.eg" } 
+	{ id: "40-3279", name: "Youssef Abo El Makarem", github_username: "youssefaboelmakarem", email:"youssef.elguindy@student.guc.edu.eg" } 
 ];
 
 app.get('/', (request, response) => {
